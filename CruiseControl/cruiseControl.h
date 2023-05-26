87bc82d90685447b71253e7b8e16c1ace7e39451
ThiagoSoutoGit <thiago.souto@yahoo.com.br>
2023-05-26 22:51:33 +1200

    cuiseControl_data documented

--- CruiseControl/cruiseControl.h
+++ CruiseControl/cruiseControl.h
@@ -1,5 +1,3 @@
-//The type 'string' is defined as 'char*' in Esterel, hence passing a string by referenc  is 'char**'
-//Aviod naming a function 'recv'
 #include <stdbool.h>
 #define boolean bool
 
--- CruiseControl/cruiseControl_data.c
+++ CruiseControl/cruiseControl_data.c
@@ -1,52 +1,69 @@
 #include <stdbool.h>
 #include "cruiseControl.h"
 
-/*
-DESCRIPTION: Saturate the throttle command to limit the acceleration.
-PARAMETERS: throttleIn - throttle input
-            saturate - true if saturated, false otherwise
-RETURNS: throttle output (ThrottleCmd)
-*/
-float saturateThrottle(float throttleIn, bool* saturate)
+/* 
+ * This function saturates the throttle input value based on predefined maximum and minimum values.
+ * If the throttle input is greater than maximum, the maximum is returned and 'saturate' is set to true.
+ * If the throttle input is less than minimum (0), 0 is returned and 'saturate' is set to true.
+ * Otherwise, the throttle input is returned and 'saturate' is set to false.
+ */
+inline float saturateThrottle(float throttleIn, bool* restrict saturate)
 {
+	// Define maximum throttle saturation value
 	static const float THROTTLESATMAX = 45.0;
+
+	// Check if throttleIn is greater than maximum
 	if (throttleIn > THROTTLESATMAX) {
 		*saturate = true;
 		return THROTTLESATMAX;
 	}
+	// Check if throttleIn is less than 0
 	else if (throttleIn < 0) {
 		*saturate = true;
 		return 0;
 	}
+	// If throttleIn is within valid range
 	else {
 		*saturate = false;
 		return throttleIn;
 	}
 }
 
-/*
-DESCRIPTION: Saturate the throttle command to limit the acceleration.
-PARAMETERS: isGoingOn - true if the cruise control has just gone into the ON state 
-                        from another state; false otherwise
-            saturate - true if saturated, false otherwise
-RETURNS: throttle output (ThrottleCmd)
-*/
+/* 
+ * This function calculates throttle value using proportional-integral (PI) controller.
+ * If cruise control is activated, it resets integral term and sets 'saturate' to true.
+ * It calculates the error term as the difference between the desired cruise speed and the actual vehicle speed.
+ * Then, the proportional and integral actions are calculated and the sum is fed to the saturateThrottle function.
+ */
 float regulateThrottle(bool isGoingOn, float cruiseSpeed, float vehicleSpeed)
 {
+	// Define proportional and integral gains
 	static const float KP = 8.113;
 	static const float KI = 0.5;
+
+	// Declare static variables for saturation status and integral term
 	static bool saturate = true;
 	static float iterm = 0;
 	
+	// Check if cruise control is activated
 	if (isGoingOn) {
-		iterm = 0;	// reset the integral action
+		// Reset integral term and set saturate to true
+		iterm = 0;	
 		saturate = true;	
 	}
+
+	// Calculate the error term
 	float error = cruiseSpeed - vehicleSpeed;
+
+	// Calculate the proportional action
 	float proportionalAction = error * KP;
-	if (saturate)
-		error = 0;	// integral action is hold when command is saturated
-	iterm = iterm + error;
+
+	// Update integral term based on saturation status
+	iterm += (saturate ? 0 : error);
+
+	// Calculate the integral action
 	float integralAction = KI * iterm;
+
+	// Return the saturated throttle value
 	return saturateThrottle(proportionalAction + integralAction, &saturate);
 }
